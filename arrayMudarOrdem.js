3435c5030a7dad85e3adcfeabc03a317558f9bd0
fabiogavazzi-magnasistemas <irlima@magnasistemas.com.br>
2022-04-26 11:35:13 -0300

    correcao

--- arrayMudarOrdem.js
+++ arrayMudarOrdem.js
@@ -1,7 +1,7 @@
 let numbers = [1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3];
 
-function changePosition(numbers, from, to) {
-    numbers.splice(to, 0, numbers.splice(from, 1)[0]);
+function changePosition(numbers, para, de) {
+    numbers.splice(de, 0, numbers.splice(para, 1)[0]);
     return numbers;
 };
 

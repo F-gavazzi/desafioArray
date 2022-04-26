3fa4a7df480b1ee2caeb77002daa02586193a1b8
fabiogavazzi-magnasistemas <irlima@magnasistemas.com.br>
2022-04-26 10:11:05 -0300

    correcao da constante

--- arrayRemoverQuinto.js
+++ arrayRemoverQuinto.js
@@ -1,3 +1,3 @@
-let numbers = [1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3];
+const numbers = [1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3];
     numbers.splice(4,1);
     console.log(numbers);

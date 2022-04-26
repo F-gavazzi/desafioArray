cb3d86bd0f29df0e984f6e0c7e8c0efb23f73f28
fabiogavazzi-magnasistemas <irlima@magnasistemas.com.br>
2022-04-26 11:28:57 -0300

    array adicionar o numero 100 e fazerele ficar na terceira posicao

---
+++ arrayAddCem.js
@@ -0,0 +1,12 @@
+let  numbers = [1, 3, 9, 12, 19, 12, 20, 40, 90, 88, 77, 4, 8, 7, 3, 4, 3];
+
+numbers.push(100);
+
+function changePosition(numbers, from, to) {
+    numbers.splice(to, 0, numbers.splice(from, 1)[0]);
+    return numbers;
+};
+let length= numbers.length;
+numbers = changePosition(numbers, numbers.length -1, 2);
+console.log(numbers);
+
